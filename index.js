db65d14d4dd80f3d1e568389a6c016c8b3240da8
kalle75 <carl.hammar@gmail.com>
2020-08-25 15:53:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,12 @@ const original = document.querySelector(".modal-img")
 previews.forEach(preview => {
       preview.addEventListener('click', ()=> {
           modal.classList.add("open");
-      })
-} )
+      });
+} );
+
+modal.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('modal')){
+        modal.classList.remove('open');
+    }
+
+});
