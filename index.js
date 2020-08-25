e6ab2e0b3994d1c39aba337247348ee2fef8d113
kalle75 <carl.hammar@gmail.com>
2020-08-25 15:42:19 +0200

    new2

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-document.querySelector(".btn").addEventListener("click" ,()=>{
-  
-});
+
+//gsap .from(".bild", {opacity: 0, duration:1, y:-50 });
+
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".gallery img");
+const original = document.querySelector(".modal-img")
+
+previews.forEach(preview => {
+      preview.addEventListener('click', ()=> {
+          modal.classList.add("open");
+      })
+} )
